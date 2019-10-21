09493e822075879243e2d39276a76261e7a278ae
The Sharp Ninja <ninja@thesharp.ninja>
2019-10-21 14:37:32 -0500

    Fixed some metadata.

--- src/C/win_x64_build.cmd
+++ src/C/win_x64_build.cmd
@@ -1,5 +1,5 @@
 echo off
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 md bin
 md bin\windows_x64
