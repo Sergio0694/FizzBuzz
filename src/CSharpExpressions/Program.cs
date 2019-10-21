c51c4081b02fa47564fe73e0c353abfd8692481e
The Sharp Ninja <ninja@thesharp.ninja>
2019-10-21 04:37:53 -0500

    FizzBuzz in Pattern-Lang

--- src/CSharpExpressions/Program.cs
+++ src/CSharpExpressions/Program.cs
@@ -1,4 +1,13 @@
-﻿#nullable enable
+﻿/*
+* FizzBuzz Implementation in C# with Compiled Expression Trees
+* The Sharp Ninja - October 20, 2019
+*
+* "Write a program that prints the numbers from 1 to 100. But for multiples of three print 
+* “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which 
+* are multiples of both three and five print “FizzBuzz”."
+*/
+
+#nullable enable
 
 using System.Linq.Expressions;
 using System.Linq;
